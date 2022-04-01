90270d2a8828d485882038c6eb8dd9435440f1da
dariwn <perezalboresd@gmail.com>
2022-04-01 15:39:17 -0600

    update

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,7 @@
+module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/confirma_asistencia/" : "/",
+};
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
