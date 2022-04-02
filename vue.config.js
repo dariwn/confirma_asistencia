24e8331847a7dca789e1f69ef3bb0b696e125b76
dariwn <perezalboresd@gmail.com>
2022-04-02 07:09:08 -0600

    up

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-module.exports = {
-  publicPath:
-    process.env.NODE_ENV === "production" ? "/confirma_asistencia/" : "/",
-};
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: ["vuetify"],
